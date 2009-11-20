d8506e2fed7dc723d71d4145f4b2fb840bb1f08d
Tom Peeters <tom@thefrontdoor.nl>
2009-11-20 17:35:26 +0000

    added components directory system and vlc server component.

---
+++ components/vlcplayer.js
@@ -0,0 +1,81 @@
+SiteFusion.ClientComponents.VlcPlayer = {
+	AssertSelf: function() {
+		// Can we display Adobe Flash movies?
+		var mimetype = navigator.mimeTypes["application/x-vlc-plugin"];
+
+		if (mimetype) {
+		   // Yes, so can we display with a plug-in?
+		   var plugin = mimetype.enabledPlugin;
+		   if (plugin) {
+					return true;
+		   } else {
+		      return false;
+		   }
+		}
+		else return false;
+	}	
+}
+
+SiteFusion.Classes.VlcPlayer = Class.create( SiteFusion.Classes.Node, {
+	sfClassName: 'VlcPlayer',
+	
+	initialize: function(win, url) {
+		this.element = document.createElementNS( 'http://www.w3.org/1999/xhtml', 'html:embed');
+		this.element.setAttribute('type', 'application/x-vlc-plugin');
+		this.element.setAttribute('version', 'VideoLAN.VLCPlugin.2');
+		this.win = win;
+		this.url = url;
+		this.setEventHost( [ 'stateChange'] );
+		this.eventHost.stateChange.msgType = 0;
+		
+		this.prevState = -1;
+		
+	},
+	afterinit: function() {
+		if (this.url)
+		{
+			this.add(this.url);
+		}
+	},
+	play: function() {
+		this.element.playlist.play();
+		
+		if (this.prevState == -1)
+			this.cycleStatusCheck();
+	},
+	
+	togglePause: function()	{
+		this.element.playlist.togglePause();
+	},
+	stop: function() {
+		this.element.playlist.stop();
+	},
+	
+	add: function(src) {
+		this.element.playlist.add(src);
+	},
+	
+	cycleStatusCheck: function() {
+		var oThis = this;
+		
+		if (this.prevState != this.element.input.state)
+		{
+			this.prevState = this.element.input.state;
+			this.fireEvent("stateChange", [this.element.input.state]);
+		}
+		window.setTimeout(function () { oThis.cycleStatusCheck();}, 100);
+	},
+	
+	sizeToContent: function()	{
+			//we can only size when there's video
+			if (this.element.input.hasVout)
+			{
+				this.element.width = this.element.video.width;
+				this.element.height = this.element.video.height;
+				
+				return true;
+			}
+			else return false;
+	}
+	
+});
