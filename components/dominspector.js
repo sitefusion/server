c82080f254136b9d20b3f59fac1d275d4e8d7c3c
Nikki Auburger <nikki@thefrontdoor.nl>
2010-01-04 13:52:54 +0000

    Added PHP counterpart for DOMInspector component

--- components/dominspector.js
+++ components/dominspector.js
@@ -14,9 +14,9 @@ SiteFusion.ClientComponents.DOMInspector = {
 	
 	DOMInspector: function() {
 		var windowDS = Components.classes["@mozilla.org/rdf/datasource;1?name=window-mediator"].getService(Components.interfaces.nsIWindowDataSource);
-	  var tmpNameSpace = {};                         
-	  var sl = Components.classes["@mozilla.org/moz/jssubscript-loader;1"].createInstance(Components.interfaces.mozIJSSubScriptLoader);
-	  sl.loadSubScript("chrome://inspector/content/hooks.js", tmpNameSpace);
-	  tmpNameSpace.inspectDOMDocument(document);
+		var tmpNameSpace = {};                         
+		var sl = Components.classes["@mozilla.org/moz/jssubscript-loader;1"].createInstance(Components.interfaces.mozIJSSubScriptLoader);
+		sl.loadSubScript("chrome://inspector/content/hooks.js", tmpNameSpace);
+		tmpNameSpace.inspectDOMDocument(document);
 	}
 }
