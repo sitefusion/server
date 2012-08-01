7a7b44a2118cb48c1d5d44b9eecabdaa717c903e
Tom Peeters <tom@thefrontdoor.nl>
2012-08-01 11:54:45 +0000

    Updated flash component

--- components/flash.js
+++ components/flash.js
@@ -5,6 +5,7 @@ SiteFusion.Classes.FlashProxy = Class.create( SiteFusion.Classes.Browser, {
 		this.element = win.createElement( 'browser' );
 		this.element.sfNode = this;
 		this.element.setAttribute('disablehistory', true);
+		this.element.setAttribute('type', 'chrome');
 		var oThis = this;
 		
 		this.hostWindow = win;
@@ -18,6 +19,9 @@ SiteFusion.Classes.FlashProxy = Class.create( SiteFusion.Classes.Browser, {
 		this.eventHost.initialized.msgType = 0;
 		this.eventHost.ready.msgType = 0;
 		this.eventHost.flashReady.msgType = 0;
+		
+		var oThis = this;
+		this.element.onload = function() { oThis.fireEvent('ready');};
 	}
 	
 });
