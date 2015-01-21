c3e2833b3b23f94111fe49f2564f164add3b63d5
Pieter Janssen <pieter.janssen@thefrontdoor.nl>
2015-01-21 11:06:40 -0500

    fixes

--- class/codeEditor.js
+++ class/codeEditor.js
@@ -83,175 +83,174 @@ SiteFusion.Classes.CodeEditor = function() {
 
     this.initialize.apply(this, arguments);
 };
-
 SiteFusion.Classes.CodeEditor.prototype = Object.create(SiteFusion.Classes.Editor.prototype);
 SiteFusion.Classes.CodeEditor.prototype.constructor = SiteFusion.Classes.CodeEditor;
     
-SiteFusion.Classes.CodeEditor.prototype.initialize = function( win ) {
-    this.element = win.createElement( 'editor' );
-    this.element.sfNode = this;
-    var oThis = this;
-    EditorListener.init(function() { oThis.handleEditorChange(); });
-    
-    this.hostWindow = win;
-    
-    this.setEventHost([
-        'before_initialize',
-        'after_loaddata',
-        'initialized',
-        'madeEditable',
-        'yield'
-    ]);
-    
-    this.eventHost.initialized.msgType = 0;
-    this.eventHost.madeEditable.msgType = 0;
-    this.eventHost.yield.msgType = 1;
-};
+    SiteFusion.Classes.CodeEditor.prototype.initialize = function( win ) {
+        this.element = win.createElement( 'editor' );
+        this.element.sfNode = this;
+        var oThis = this;
+        EditorListener.init(function() { oThis.handleEditorChange(); });
+        
+        this.hostWindow = win;
+        
+        this.setEventHost([
+            'before_initialize',
+            'after_loaddata',
+            'initialized',
+            'madeEditable',
+            'yield'
+        ]);
+        
+        this.eventHost.initialized.msgType = 0;
+        this.eventHost.madeEditable.msgType = 0;
+        this.eventHost.yield.msgType = 1;
+    };
 
-SiteFusion.Classes.CodeEditor.prototype.handleEditorChange = function() {
-    
-};
-
-SiteFusion.Classes.CodeEditor.prototype.makeEditable = function() {
-    this.element.makeEditable("plaintext", false);
-    this.fireEvent( 'madeEditable' );
-};
-
-SiteFusion.Classes.CodeEditor.prototype.editorLoaded = function(contents) {
-    if (!this.editorElement) {
-        this.editorElement = this.element;
-    }
-
-    var editor = null;
-    var textEditor = null;
-
-    var oThis = this;
-    
-    var getEditorFunc = function() {
-        setTimeout(function() {
-
-            if (oThis.element.editingSession) {
-                oThis.element.editingSession.makeWindowEditable(oThis.element.contentWindow, "plaintext", false, true, false);
-                textEditor = oThis.element.editingSession.getEditorForWindow(oThis.element.contentWindow);
-                if (textEditor) {
-                    editor = textEditor.QueryInterface(Components.interfaces.nsIHTMLEditor);
-                }
-
-                if (editor && textEditor) {         
-                    oThis.htmlEditor = editor;
-                    
-                    oThis.textEditor = textEditor.QueryInterface(Ci.nsIPlaintextEditor);
-                    oThis.textEditor.enableUndo(true);
-                    oThis.textEditor.rootElement.style.fontFamily = "-moz-fixed";
-                    oThis.textEditor.rootElement.style.fontSize = "10pt";
-                    oThis.textEditor.rootElement.style.backgroundColor = "white";
-                    oThis.textEditor.rootElement.style.whiteSpace = "nowrap";
-                    oThis.textEditor.rootElement.style.marginLeft = 5;
-                    
-                    oThis.element.contentWindow.onfocus = function() {
-                        var myKey = oThis.hostWindow.createElement( 'key' );
-                        oThis.element.myKeySet = oThis.hostWindow.createElement( 'keyset' );
-                        myKey.setAttribute('key', "V");
-                        myKey.setAttribute('modifiers', "accel");
-                
-                        myKey.sfNode = oThis;
-                        
-                        myKey.setAttribute("oncommand", "sfRootWindow.windowObject.SiteFusion.Registry[" + oThis.cid + "].textEditor.QueryInterface(Components.interfaces.nsIHTMLEditor).pasteNoFormatting(1)");
-                
-                        oThis.element.myKeySet.appendChild(myKey);
-                        oThis.element.appendChild(oThis.element.myKeySet);
-                    };
-                    oThis.element.contentWindow.onblur = function() {
-                        oThis.element.removeChild(oThis.element.myKeySet);
-                    };
-                    
-                    //dirty hack for Mozilla scroll-in-contentEditable-bug  
-                    //todo: keep track of Mozilla development at: https://developer.mozilla.org/en/DOM/Selection/modify
-                    oThis.element.contentWindow.onkeypress = function(event) {
-                            if (event.keyCode >= 37 && event.keyCode <= 40) {
-                                event.preventDefault();
-                                var selection = oThis.element.contentWindow.getSelection();
-                                switch (event.keyCode) {
-                                    case 37:
-                                    //left arrow
-                                        selection.modify((event.shiftKey ? "extend": "move"), "backward", "character");
-                                    break;
-                                    case 38:
-                                    //up arrow
-                                        selection.modify((event.shiftKey ? "extend": "move"), "backward", "line");
-                                    break;
-                                    case 39:
-                                    //right arrow
-                                        selection.modify((event.shiftKey ? "extend": "move"), "forward", "character");
-                                    break;
-                                    case 40:
-                                    //down arrow
-                                        selection.modify((event.shiftKey ? "extend": "move"), "forward", "line");
-                                    break;
-                                }
-                            }
-                    };
-                    
-                    //set language css
-                    oThis.applyStyling();
-                
-                    EditorListener.attach(oThis.textEditor);
-                    oThis.setSource(contents);
-                    oThis.fireEvent( 'after_loaddata' );
-                } 
-            }
-            //are we completely ready yet?
-            if (!oThis.element.editingSession || !editor || !textEditor) {
-                getEditorFunc();
-            }
-        }, 20);
+    SiteFusion.Classes.CodeEditor.prototype.handleEditorChange = function() {
         
     };
 
-    getEditorFunc();
-};
+    SiteFusion.Classes.CodeEditor.prototype.makeEditable = function() {
+        this.element.makeEditable("plaintext", false);
+        this.fireEvent( 'madeEditable' );
+    };
 
-SiteFusion.Classes.CodeEditor.prototype.clearSource = function() {
-    var textEditor = this.textEditor;
-    textEditor.enableUndo(false);
-    textEditor.selectAll();
+    SiteFusion.Classes.CodeEditor.prototype.editorLoaded = function(contents) {
+        if (!this.editorElement) {
+            this.editorElement = this.element;
+        }
 
-    try {
-        textEditor.deleteSelection(textEditor.eNone, true);
-    }
-    catch (e) {
-        textEditor.deleteSelection(textEditor.eNone);
-    }
+        var editor = null;
+        var textEditor = null;
 
-    textEditor.enableUndo(true);
-    textEditor.resetModificationCount();
+        var oThis = this;
+        
+        var getEditorFunc = function() {
+            setTimeout(function() {
 
-    textEditor.document.title = "";
+                if (oThis.element.editingSession) {
+                    oThis.element.editingSession.makeWindowEditable(oThis.element.contentWindow, "plaintext", false, true, false);
+                    textEditor = oThis.element.editingSession.getEditorForWindow(oThis.element.contentWindow);
+                    if (textEditor) {
+                        editor = textEditor.QueryInterface(Components.interfaces.nsIHTMLEditor);
+                    }
 
-    this.element.contentWindow.focus();
-};
+                    if (editor && textEditor) {         
+                        oThis.htmlEditor = editor;
+                        
+                        oThis.textEditor = textEditor.QueryInterface(Ci.nsIPlaintextEditor);
+                        oThis.textEditor.enableUndo(true);
+                        oThis.textEditor.rootElement.style.fontFamily = "-moz-fixed";
+                        oThis.textEditor.rootElement.style.fontSize = "10pt";
+                        oThis.textEditor.rootElement.style.backgroundColor = "white";
+                        oThis.textEditor.rootElement.style.whiteSpace = "nowrap";
+                        oThis.textEditor.rootElement.style.marginLeft = 5;
+                        
+                        oThis.element.contentWindow.onfocus = function() {
+                            var myKey = oThis.hostWindow.createElement( 'key' );
+                            oThis.element.myKeySet = oThis.hostWindow.createElement( 'keyset' );
+                            myKey.setAttribute('key', "V");
+                            myKey.setAttribute('modifiers', "accel");
+                    
+                            myKey.sfNode = oThis;
+                            
+                            myKey.setAttribute("oncommand", "sfRootWindow.windowObject.SiteFusion.Registry[" + oThis.cid + "].textEditor.QueryInterface(Components.interfaces.nsIHTMLEditor).pasteNoFormatting(1)");
+                    
+                            oThis.element.myKeySet.appendChild(myKey);
+                            oThis.element.appendChild(oThis.element.myKeySet);
+                        };
+                        oThis.element.contentWindow.onblur = function() {
+                            oThis.element.removeChild(oThis.element.myKeySet);
+                        };
+                        
+                        //dirty hack for Mozilla scroll-in-contentEditable-bug  
+                        //todo: keep track of Mozilla development at: https://developer.mozilla.org/en/DOM/Selection/modify
+                        oThis.element.contentWindow.onkeypress = function(event) {
+                                if (event.keyCode >= 37 && event.keyCode <= 40) {
+                                    event.preventDefault();
+                                    var selection = oThis.element.contentWindow.getSelection();
+                                    switch (event.keyCode) {
+                                        case 37:
+                                        //left arrow
+                                            selection.modify((event.shiftKey ? "extend": "move"), "backward", "character");
+                                        break;
+                                        case 38:
+                                        //up arrow
+                                            selection.modify((event.shiftKey ? "extend": "move"), "backward", "line");
+                                        break;
+                                        case 39:
+                                        //right arrow
+                                            selection.modify((event.shiftKey ? "extend": "move"), "forward", "character");
+                                        break;
+                                        case 40:
+                                        //down arrow
+                                            selection.modify((event.shiftKey ? "extend": "move"), "forward", "line");
+                                        break;
+                                    }
+                                }
+                        };
+                        
+                        //set language css
+                        oThis.applyStyling();
+                    
+                        EditorListener.attach(oThis.textEditor);
+                        oThis.setSource(contents);
+                        oThis.fireEvent( 'after_loaddata' );
+                    } 
+                }
+                //are we completely ready yet?
+                if (!oThis.element.editingSession || !editor || !textEditor) {
+                    getEditorFunc();
+                }
+            }, 20);
+            
+        };
 
-SiteFusion.Classes.CodeEditor.prototype.getSource = function() {
-    var textEditor = this.textEditor;
-    var source = textEditor.outputToString("text/plain", 1028);
-    return source;
-};
+        getEditorFunc();
+    };
 
-SiteFusion.Classes.CodeEditor.prototype.setSource = function(aText) {
-    this.clearSource();
+    SiteFusion.Classes.CodeEditor.prototype.clearSource = function() {
+        var textEditor = this.textEditor;
+        textEditor.enableUndo(false);
+        textEditor.selectAll();
 
-    this.insertText(aText);
-};
+        try {
+            textEditor.deleteSelection(textEditor.eNone, true);
+        }
+        catch (e) {
+            textEditor.deleteSelection(textEditor.eNone);
+        }
 
-SiteFusion.Classes.CodeEditor.prototype.applyStyling = function () {
-    this.htmlEditor.replaceHeadContentsWithHTML('<style>body { padding-left: 25px; padding-top: 0px; background: white; margin-left: 32px; font-family: monospace; font-size: 13px; background-repeat: repeat-y; background-position :0 3px; line-height: 16px; height: 95%; } P { margin: 0; padding: 0; border: 0; outline: 0; display: block; }</style>'); 
-    this.element.contentDocument.body.style.backgroundImage = "url('" + this.parseImageURL('/class/res/line-numbers.png') + "')";
-};
+        textEditor.enableUndo(true);
+        textEditor.resetModificationCount();
 
-SiteFusion.Classes.CodeEditor.prototype.insertText = function(aText) {
-    this.textEditor.insertText(aText);
-};
+        textEditor.document.title = "";
 
-SiteFusion.Classes.CodeEditor.prototype.yield = function() {
-    this.fireEvent( 'yield', [ this.getSource() ] );
-};
+        this.element.contentWindow.focus();
+    };
+
+    SiteFusion.Classes.CodeEditor.prototype.getSource = function() {
+        var textEditor = this.textEditor;
+        var source = textEditor.outputToString("text/plain", 1028);
+        return source;
+    };
+
+    SiteFusion.Classes.CodeEditor.prototype.setSource = function(aText) {
+        this.clearSource();
+
+        this.insertText(aText);
+    };
+
+    SiteFusion.Classes.CodeEditor.prototype.applyStyling = function () {
+        this.htmlEditor.replaceHeadContentsWithHTML('<style>body { padding-left: 25px; padding-top: 0px; background: white; margin-left: 32px; font-family: monospace; font-size: 13px; background-repeat: repeat-y; background-position :0 3px; line-height: 16px; height: 95%; } P { margin: 0; padding: 0; border: 0; outline: 0; display: block; }</style>'); 
+        this.element.contentDocument.body.style.backgroundImage = "url('" + this.parseImageURL('/class/res/line-numbers.png') + "')";
+    };
+
+    SiteFusion.Classes.CodeEditor.prototype.insertText = function(aText) {
+        this.textEditor.insertText(aText);
+    };
+
+    SiteFusion.Classes.CodeEditor.prototype.yield = function() {
+        this.fireEvent( 'yield', [ this.getSource() ] );
+    };
